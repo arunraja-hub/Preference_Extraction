e364f71f6e692dfea08fce79c5b6e15871d8ee3d
Nevan Wichers <wichersn@google.com>
2020-07-11 16:13:10 +0700

    Add env resize option

--- agent/rl_env/DoomEnviroment.py
+++ agent/rl_env/DoomEnviroment.py
@@ -5,9 +5,12 @@ import numpy as np
 from tf_agents.environments import py_environment, utils
 from tf_agents.specs import array_spec
 from tf_agents.trajectories import time_step as ts
+from tf_agents.environments import wrappers
 from vizdoom import DoomGame
 from vizdoom import GameVariable
+import matplotlib
 import gin
+import random
 
 """
     DoomEnviroment Class, code adapted from
@@ -17,8 +20,10 @@ import gin
 @gin.configurable
 class DoomEnvironment(py_environment.PyEnvironment):
 
-    def __init__(self, config_name, episode_timeout=1000, timeout_channel=True, ammo_channel=True):
+    def __init__(self, config_name, episode_timeout=1000, obs_shape = (60, 100), timeout_channel=True, ammo_channel=True):
         super().__init__()
+
+        self.obs_shape = obs_shape
 
         self._game = self.configure_doom(config_name, episode_timeout, timeout_channel)
         self.timeout_channel = timeout_channel
@@ -34,7 +39,7 @@ class DoomEnvironment(py_environment.PyEnvironment):
         if not ammo_channel:
             all_channels -= 1
         self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(84, 84, all_channels), dtype=np.float32, minimum=0, maximum=1, name='observation')
+            shape=(self.obs_shape[0], self.obs_shape[1], all_channels), dtype=np.float32, minimum=0, maximum=1, name='observation')
         
     @staticmethod
     def configure_doom(config_name, episode_timeout, timeout_channel):
@@ -83,12 +88,13 @@ class DoomEnvironment(py_environment.PyEnvironment):
         """
             Preprocess frame for agent by:
             - cutout interesting square part of screen
-            - downsample cutout to 84x84 (same as used for atari games)
+            - downsample cutout to self.obs_shape (same as used for atari games)
             - normalize images to interval [0,1]
         """
         frame = self.get_screen_buffer_frame()
-        cutout = frame[10:-10, 30:-30]
-        resized = cv2.resize(cutout, (84, 84))
+        frame = frame[40:-30, :]
+        # The cv2 dims are backwards
+        resized = cv2.resize(frame, (self.obs_shape[1], self.obs_shape[0]))
         if self.timeout_channel:
             resized = np.dstack((resized, self.get_remaining_time_channel()))
         if self.ammo_channel:
@@ -96,16 +102,16 @@ class DoomEnvironment(py_environment.PyEnvironment):
         return np.divide(resized, 255, dtype=np.float32)
 
     def get_remaining_time_channel(self):
-        time_channel = np.zeros((84 * 84))
+        time_channel = np.zeros(self.obs_shape[0] * self.obs_shape[1])
         time_left = self._game.get_episode_timeout() - self._game.get_episode_time()
         time_channel[:time_left * 10] = 1
-        return time_channel.reshape((84, 84))
+        return time_channel.reshape(self.obs_shape)
 
     def get_remaining_ammo_channel(self):
-        ammo_channel = np.zeros((84 * 84))
+        ammo_channel = np.zeros(self.obs_shape[0] * self.obs_shape[1])
         ammo_left = self.get_weapon_remaining_ammo()
         ammo_channel[:ammo_left * 10] = 1
-        return ammo_channel.reshape((84, 84))
+        return ammo_channel.reshape(self.obs_shape)
 
     def get_weapon_remaining_ammo(self):
         for am_ix, ammo in enumerate([GameVariable.AMMO1, GameVariable.AMMO2, GameVariable.AMMO3,
@@ -122,10 +128,41 @@ class DoomEnvironment(py_environment.PyEnvironment):
         else:
             return np.rollaxis(self._game.get_state().screen_buffer, 0, 3)
 
+class SaveStateWrapper(wrappers.PyEnvironmentBaseWrapper):
+    def __init__(self, env, path, save_prob):
+        super(SaveStateWrapper, self).__init__(env)
+        self.path = path
+        self.save_prob = save_prob
+        os.makedirs(self.path, exist_ok=True)
+
+        self.save_num = 0
+
+    def _step(self, action):
+        time_step = self._env.step(action)
+
+        if random.random() < self.save_prob:
+
+            cv2.imwrite(os.path.join(self.path, str(self.save_num)+'_0.png'), (time_step.observation[:, :, :3]* 255).astype(int))
+
+            for i in range(3, time_step.observation.shape[2]):
+                print("i", i)
+                print(np.min(time_step.observation[:, :, i]))
+                print(np.max(time_step.observation[:, :, i]))
+                cv2.imwrite(os.path.join(self.path, str(self.save_num) + '_' + str(i) + '.png'),
+                            (time_step.observation[:, :, i] * 255).astype(int))
+
+            self.save_num += 1
+
+        return time_step
+
+
+
 @gin.configurable
 def tf_agents_env(_):
     return DoomEnvironment()
 
 if __name__ == "__main__":
-	environment = DoomEnvironment('custom.cfg')
-	utils.validate_py_environment(environment, episodes=5)
+    environment = SaveStateWrapper(DoomEnvironment('custom.cfg'), 'saved_env_states', .01)
+
+
+    utils.validate_py_environment(environment, episodes=5)
