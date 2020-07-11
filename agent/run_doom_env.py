dddbd259b7944eb91feb9975883a19a1e58f21ac
ricvolpe <volpatorv@gmail.com>
2020-07-11 18:10:12 +0000

    Clean up

--- agent/run_doom_env.py
+++ agent/run_doom_env.py
@@ -7,6 +7,6 @@ import gin.tf.external_configurables
 if __name__ == "__main__":
     gin.parse_config_files_and_bindings(['configs/dqn.gin'], '', skip_unknown=True)
 
-    environment = SaveStateWrapper(tf_agents_env(None), 'saved_env_states', 1.0)
+    environment = SaveStateWrapper(tf_agents_env(None), 'saved_env_states', 0.1)
 
-    utils.validate_py_environment(environment, episodes=1)
+    utils.validate_py_environment(environment, episodes=5)
