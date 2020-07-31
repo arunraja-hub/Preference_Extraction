45c0edf5294f900a640186c421a6d18784bea8ea
ricvolpe <volpatorv@gmail.com>
2020-07-31 16:57:08 +0200

    New reward params in scenario

--- agent/rl_env/play_game_locally.py
+++ agent/rl_env/play_game_locally.py
@@ -4,11 +4,11 @@ from vizdoom import *
 game = DoomGame()
 game.load_config("custom.cfg")
 game.set_labels_buffer_enabled(True)
-game.set_episode_timeout(1000)
+game.set_episode_timeout(2000)
 game.set_mode(Mode.SPECTATOR)
 game.init()
 
-episodes = 10
+episodes = 5
 
 for i in range(episodes):
     game.new_episode()
@@ -17,6 +17,5 @@ for i in range(episodes):
         ticker += 1
         state = game.get_state()
         game.advance_action()
-        print(ticker, game.get_total_reward())
     r = game.get_total_reward()
     print("Reward", r)
