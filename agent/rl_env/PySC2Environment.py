ea9c7b457bb6fdb04f8e4f386f231e49c3f81dad
Mislav Jurić <mislav.juric01@gmail.com>
2020-09-09 13:46:55 +0200

    Added methods that must be overridden for TF-Agents wrapper

--- agent/rl_env/PySC2Environment.py
+++ agent/rl_env/PySC2Environment.py
@@ -6,5 +6,20 @@ from tf_agents.trajectories import time_step as ts
 import pysc2
 
 class PySC2Environment(py_environment.PyEnvironment):
-     def __init__(self):
+    def __init__(self):
         super().__init__()
+        pass
+
+    def action_spec(self):
+        #return self._action_spec
+        pass
+
+    def observation_spec(self):
+        #return self._observation_spec
+        pass
+
+    def _reset(self):
+        pass
+
+    def _step(self, action):
+        pass
