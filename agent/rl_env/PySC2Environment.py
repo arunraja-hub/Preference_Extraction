7d51def8717b71a9b3f87a9eeb37691d0365011a
Mislav Jurić <mislav.juric01@gmail.com>
2020-09-15 17:17:51 +0200

    Modified PySC2 TF-Agents wrapper

--- agent/rl_env/PySC2Environment.py
+++ agent/rl_env/PySC2Environment.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 from tf_agents.environments import py_environment
 from tf_agents.environments import wrappers
@@ -5,21 +9,111 @@ from tf_agents.specs import array_spec
 from tf_agents.trajectories import time_step as ts
 import pysc2
 
+import importlib
+import threading
+
+from absl import app
+from absl import flags
+from future.builtins import range  # pylint: disable=redefined-builtin
+
+from pysc2 import maps
+from pysc2.env import available_actions_printer
+from pysc2.env import run_loop
+from pysc2.env import sc2_env
+from pysc2.lib import point_flag
+from pysc2.lib import stopwatch
+
+# based on https://github.com/deepmind/pysc2/blob/master/pysc2/bin/agent.py and https://github.com/deepmind/pysc2/blob/master/pysc2/env/run_loop.py
+
 class PySC2Environment(py_environment.PyEnvironment):
     def __init__(self):
         super().__init__()
-        pass
+
+        # TODO: How do I represent the action, which has C-style function calls? (https://github.com/deepmind/pysc2/blob/master/docs/environment.md#actions)
+        
+        # general player information observation (https://github.com/deepmind/pysc2/blob/master/docs/environment.md#general-player-information)
+        self._observation_spec = array_spec.ArraySpec(
+            shape=(11), dtype=np.float32, name='observation')
+
+        self.env = setup_env(self)
+    
 
     def action_spec(self):
-        #return self._action_spec
-        pass
+        return self._action_spec
 
     def observation_spec(self):
-        #return self._observation_spec
-        pass
+        return self._observation_spec
 
     def _reset(self):
-        pass
+        return self.env.reset()
 
     def _step(self, action):
-        pass
+        return self.env.step(action)
+
+    def setup_env(self):
+        FLAGS = flags.FLAGS
+        flags.DEFINE_bool("render", True, "Whether to render with pygame.")
+        point_flag.DEFINE_point("feature_screen_size", "84",
+                                "Resolution for screen feature layers.")
+        point_flag.DEFINE_point("feature_minimap_size", "64",
+                                "Resolution for minimap feature layers.")
+        point_flag.DEFINE_point("rgb_screen_size", None,
+                                "Resolution for rendered screen.")
+        point_flag.DEFINE_point("rgb_minimap_size", None,
+                                "Resolution for rendered minimap.")
+        flags.DEFINE_enum("action_space", None, sc2_env.ActionSpace._member_names_,  # pylint: disable=protected-access
+                          "Which action space to use. Needed if you take both feature "
+                          "and rgb observations.")
+        flags.DEFINE_bool("use_feature_units", False,
+                          "Whether to include feature units.")
+        flags.DEFINE_bool("use_raw_units", False,
+                          "Whether to include raw units.")
+        flags.DEFINE_bool("disable_fog", False, "Whether to disable Fog of War.")
+
+        flags.DEFINE_integer("max_agent_steps", 0, "Total agent steps.")
+        flags.DEFINE_integer("game_steps_per_episode", None, "Game steps per episode.")
+        flags.DEFINE_integer("max_episodes", 0, "Total episodes.")
+        flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
+
+        flags.DEFINE_string("agent", "pysc2.agents.random_agent.RandomAgent",
+                    "Which agent to run, as a python path to an Agent class.")
+        flags.DEFINE_string("agent_name", None,
+                    "Name of the agent in replays. Defaults to the class name.")
+        flags.DEFINE_enum("agent_race", "random", sc2_env.Race._member_names_,  # pylint: disable=protected-access
+                  "Agent 1's race.")
+
+        flags.DEFINE_string("agent2", "Bot", "Second agent, either Bot or agent class.")
+        flags.DEFINE_string("agent2_name", None,
+                    "Name of the agent in replays. Defaults to the class name.")
+        flags.DEFINE_enum("agent2_race", "random", sc2_env.Race._member_names_,  # pylint: disable=protected-access
+                  "Agent 2's race.")
+        flags.DEFINE_enum("difficulty", "very_easy", sc2_env.Difficulty._member_names_,  # pylint: disable=protected-access
+                  "If agent2 is a built-in Bot, it's strength.")
+        flags.DEFINE_enum("bot_build", "random", sc2_env.BotBuild._member_names_,  # pylint: disable=protected-access
+                  "Bot's build strategy.")
+
+        flags.DEFINE_bool("profile", False, "Whether to turn on code profiling.")
+        flags.DEFINE_bool("trace", False, "Whether to trace the code execution.")
+        flags.DEFINE_integer("parallel", 1, "How many instances to run in parallel.")
+
+        flags.DEFINE_bool("save_replay", True, "Whether to save a replay at the end.")
+
+        flags.DEFINE_string("map", None, "Name of a map to use.")
+        flags.DEFINE_bool("battle_net_map", False, "Use the battle.net map version.")
+        flags.mark_flag_as_required("map")
+
+        return sc2_env.SC2Env(map_name=map_name,
+                              battle_net_map=FLAGS.battle_net_map,
+                              players=players,
+                              agent_interface_format=sc2_env.parse_agent_interface_format(
+                                  feature_screen=FLAGS.feature_screen_size,
+                                  feature_minimap=FLAGS.feature_minimap_size,
+                                  rgb_screen=FLAGS.rgb_screen_size,
+                                  rgb_minimap=FLAGS.rgb_minimap_size,
+                                  action_space=FLAGS.action_space,
+                                  use_feature_units=FLAGS.use_feature_units,
+                                  use_raw_units=FLAGS.use_raw_units),
+                              step_mul=FLAGS.step_mul,
+                              game_steps_per_episode=FLAGS.game_steps_per_episode,
+                              disable_fog=FLAGS.disable_fog,
+                              visualize=visualize)
