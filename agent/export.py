28281306099f3c17d1bc0eb930fc0e162c338298
ricvolpe <volpatorv@hotmail.com>
2020-10-10 15:20:20 +0000

    Progressed with agent exporter

--- agent/export.py
+++ agent/export.py
@@ -12,11 +12,9 @@ import gin.tf.external_configurables
 import exporter
 
 flags.DEFINE_string('root_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
-                    'Root directory for writing logs/summaries/checkpoints.')
-flags.DEFINE_multi_string('gin_file', "",
-                          'Paths to the study config files.')
-flags.DEFINE_bool('add_root_dir_gin_file', False, "")
-flags.DEFINE_multi_string('gin_bindings', None, 'Gin binding to pass through.')
+                    'Root directory for reading logs/summaries/checkpoints.')
+flags.DEFINE_multi_string('gin_file', "", 'Paths to the study config files.')
+flags.DEFINE_string('checkpoint', None, 'Checkpoint at which export agent')
 FLAGS = flags.FLAGS
 
 def main(_):
@@ -25,17 +23,15 @@ def main(_):
     tf.compat.v2.enable_v2_behavior()
     
     gin_file = FLAGS.gin_file
-    if FLAGS.add_root_dir_gin_file:
-        gin_file.append(os.path.join(FLAGS.root_dir, "train/operative_config-0.gin"))
-        
-    gin.parse_config_files_and_bindings(gin_file, FLAGS.gin_bindings, skip_unknown=True)
-    
+    gin_file.append(os.path.join(FLAGS.root_dir, "train/operative_config-0.gin"))
+    gin.parse_config_files_and_bindings(gin_file, None, skip_unknown=True)
     root_dir = FLAGS.root_dir
+    checkpoint_no = FLAGS.checkpoint
     
     with gin.unlock_config():
         gin.bind_parameter('%ROOT_DIR', root_dir)
 
-    agent_exporter = exporter.Exporter(root_dir)
+    agent_exporter = exporter.Exporter(root_dir, checkpoint_no, verify_restore_success=True)
 
 if __name__ == '__main__':
     flags.mark_flag_as_required('root_dir')
--- agent/exporter.py
+++ agent/exporter.py
@@ -1,14 +1,47 @@
+import os
+import numpy as np
+
 import gin
+import tensorflow as tf
 
 from tf_agents.environments import py_environment
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
+from tf_agents.agents.dqn.dqn_agent import DqnAgent
+from tf_agents.agents.ppo.ppo_kl_penalty_agent import PPOKLPenaltyAgent
 import environment_specs
 
+
+def extract_weights_sample(agent, agent_class):
+    if isinstance(agent, DqnAgent):
+        return agent._q_network.layers[0].layers[0].get_weights()[0].copy()
+    elif isinstance(agent, PPOKLPenaltyAgent):
+        return agent.actor_net.layers[0].layers[0].get_weights()[0].copy()
+
+def flatten_model(model_nested):
+    def get_layers(layers):
+        layers_flat = []
+        for layer in layers:
+            try:
+                layers_flat.extend(get_layers(layer.layers))
+            except AttributeError:
+                layers_flat.append(layer)
+        return layers_flat
+
+    model_flat = tf.keras.models.Sequential(get_layers(model_nested.layers))
+    
+    return model_flat
+    
 @gin.configurable
 class Exporter(object):
     
-    def __init__(self, root_dir, env_load_fn=suite_gym.load, env_name='CartPole-v0', agent_class=None):
+    def __init__(self, 
+                 root_dir, 
+                 checkpoint_no=None, 
+                 env_load_fn=suite_gym.load, 
+                 env_name='CartPole-v0', 
+                 agent_class=None,
+                 verify_restore_success=True):
         
         if not agent_class:
             raise ValueError('The `agent_class` parameter of Exporter must be set.')
@@ -27,5 +60,31 @@ class Exporter(object):
         environment_specs.set_action_spec(self._tf_env.action_spec())
         
         self._agent = agent_class(self._tf_env.time_step_spec(), self._tf_env.action_spec())
+        if verify_restore_success:
+            pre_restore_weigths = extract_weights_sample(self._agent, agent_class)
         
-        print(self._agent)
+        checkpoint_dir = os.path.join(root_dir, 'train', 'policy')
+        checkpoint_state = tf.train.get_checkpoint_state(checkpoint_dir)
+        checkpoint_paths = list(checkpoint_state.all_model_checkpoint_paths)
+        if checkpoint_no is None:
+            checkpoint = checkpoint_paths[-1]
+        else:
+            checkpoint = [x for x in checkpoint_paths if str(checkpoint_no) in x][0]
+        
+        policy_checkpoint = tf.train.Checkpoint(policy=self._agent.policy)
+        load_status = policy_checkpoint.restore(checkpoint).expect_partial()
+        load_status.initialize_or_restore()
+        
+        if verify_restore_success:
+            post_restore_weigths = extract_weights_sample(self._agent, agent_class)
+            post_equal_pre = np.allclose(pre_restore_weigths, post_restore_weigths, rtol=1e-05, atol=1e-08)
+            assert post_equal_pre is False, 'Checkpoint restoring did not change weigths'
+            
+        if isinstance(self._agent, DqnAgent):
+            model = flatten_model(self._agent._q_network) 
+        elif isinstance(self._agent, PPOKLPenaltyAgent):
+            model = flatten_model(self._agent.actor_net)
+        
+        model.build(input_shape=(1,) + env._observation_spec.shape)
+        model.summary()
+        model.save(os.path.join(root_dir, 'saved_model'))
